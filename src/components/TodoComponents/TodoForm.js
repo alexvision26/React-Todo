38c3d184d4e6f3df5aefa76efe322dbf9b8619e6
Alex Miller <alexmillerprojects@gmail.com>
2020-02-10 13:45:21 -0800

    added form functionality to add new tasks

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            newTodo: '',
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+          newTodo: e.target.value
+        });
+      };
+
+      handleSubmit = e => {
+          e.preventDefault();
+          this.props.addNewTask(this.state.newTodo);
+          this.setState({ newTodo: '' })
+
+      }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' name='newTodo' value={this.state.newTodo} onChange={this.handleChange} />
+                    <button>Add</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
+import React from 'react';
+
+import Todo from './Todo';
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+const TodoList = props => {
+    console.log(props.todoList)
+    return (
+        <>
+        {props.todoList.map((item) => (
+            <Todo key={item.id} item={item}/>
+        ))}
+        </>
+    )
+}
+
+export default TodoList;
